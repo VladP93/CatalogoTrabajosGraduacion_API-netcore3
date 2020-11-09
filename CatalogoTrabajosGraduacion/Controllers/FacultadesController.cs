1549e86d34824e3e99b22ff3e3c36f8dff33618b
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-11-09 00:08:02 -0600

    Net Core API layout

---
+++ CatalogoTrabajosGraduacion/Controllers/FacultadesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CatalogoTrabajosGraduacion.Models;
+
+namespace CatalogoTrabajosGraduacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacultadesController : ControllerBase
+    {
+        private readonly trabajosGraduacionContext _context;
+
+        public FacultadesController(trabajosGraduacionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Facultades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Facultad>>> GetFacultad()
+        {
+            return await _context.Facultad.ToListAsync();
+        }
+
+        // GET: api/Facultades/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Facultad>> GetFacultad(int id)
+        {
+            var facultad = await _context.Facultad.FindAsync(id);
+
+            if (facultad == null)
+            {
+                return NotFound();
+            }
+
+            return facultad;
+        }
+
+        // PUT: api/Facultades/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFacultad(int id, Facultad facultad)
+        {
+            if (id != facultad.IdFacultad)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(facultad).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacultadExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Facultades
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Facultad>> PostFacultad(Facultad facultad)
+        {
+            _context.Facultad.Add(facultad);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFacultad", new { id = facultad.IdFacultad }, facultad);
+        }
+
+        // DELETE: api/Facultades/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Facultad>> DeleteFacultad(int id)
+        {
+            var facultad = await _context.Facultad.FindAsync(id);
+            if (facultad == null)
+            {
+                return NotFound();
+            }
+
+            _context.Facultad.Remove(facultad);
+            await _context.SaveChangesAsync();
+
+            return facultad;
+        }
+
+        private bool FacultadExists(int id)
+        {
+            return _context.Facultad.Any(e => e.IdFacultad == id);
+        }
+    }
+}
